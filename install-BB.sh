e7fc17026a6523008e9c9e6dccf1c5195f01de07
caskeychr <caskey_christopher@bah.com>
2022-06-30 12:09:36 -0500

    updates

--- install-BB.sh
+++ install-BB.sh
@@ -6,7 +6,7 @@ kubectl create namespace bigbang
 # export fp=$(gpg --list-keys --fingerprint | grep "bigbang-dev-environment" -B 1 | grep -v "bigbang-dev-environment" | tr -d ' ' | tr -d 'Keyfingerprint=')
 gpg --export-secret-key --armor ${fp} | kubectl create secret generic sops-gpg -n bigbang --from-file=bigbangkey.asc=/dev/stdin
 
-kubectl create secret -n bigbang generic repository-credentials --from-literal=username=$CSN_GITHUB_USER --from-literal=password=$CSN_GITHUB_TOKEN
+kubectl create secret -n bigbang generic private-git --from-literal=username=$CSN_GITHUB_USER --from-literal=password=$CSN_GITHUB_PASS
 
 kubectl apply -f dev/bigbang.yaml
 
--- install-flux.sh
+++ install-flux.sh
@@ -3,7 +3,7 @@
 
 kubectl create namespace flux-system
 
-kubectl create secret docker-registry private-registry --docker-server=https://registry1.dso.mil --docker-username=$REGISTRY1_USER --docker-password=$REGISTRY1_PASS -n flux-system
+kubectl create secret docker-registry private-registry --docker-server=registry1.dso.mil --docker-username=$REGISTRY1_USER --docker-password=$REGISTRY1_PASS -n flux-system
 
 kustomize build https://repo1.dso.mil/platform-one/big-bang/bigbang.git//base/flux?ref=1.31.0 | kubectl apply -f -
 
